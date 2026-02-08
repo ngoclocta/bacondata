1db1ebd4a74ca116e66e76095a9afdb643e6d792
bacon🥓 <ngoclocta6@gmail.com>
2026-02-08 14:04:47 +0700

    Add Cloudflare tunnel setup to bacon.sh
    Added Cloudflare tunnel setup and installation instructions.

--- bacon.sh
+++ bacon.sh
@@ -1,7 +1,9 @@
 #Downloads
 curl -s -o login.sh -L "https://raw.githubusercontent.com/JohnnyNetsec/github-vm/main/mac/login.sh"
+
 #disable spotlight indexing
 sudo mdutil -i off -a
+
 #Create new account
 sudo dscl . -create /Users/runneradmin
 sudo dscl . -create /Users/runneradmin UserShell /bin/bash
@@ -13,14 +15,20 @@ sudo dscl . -passwd /Users/runneradmin P@ssw0rd!
 sudo dscl . -passwd /Users/runneradmin P@ssw0rd!
 sudo createhomedir -c -u runneradmin > /dev/null
 sudo dscl . -append /Groups/admin GroupMembership runneradmin
+
 #Enable VNC
 sudo /System/Library/CoreServices/RemoteManagement/ARDAgent.app/Contents/Resources/kickstart -activate -configure -allowAccessFor -allUsers -privs -all
 sudo /System/Library/CoreServices/RemoteManagement/ARDAgent.app/Contents/Resources/kickstart -configure -clientopts -setvnclegacy -vnclegacy yes 
 echo runnerrdp | perl -we 'BEGIN { @k = unpack "C*", pack "H*", "1734516E8BA8C5E2FF1C39567390ADCA"}; $_ = <>; chomp; s/^(.{8}).*/$1/; @p = unpack "C*", $_; foreach (@k) { printf "%02X", $_ ^ (shift @p || 0) }; print "\n"' | sudo tee /Library/Preferences/com.apple.VNCSettings.txt
+
 #Start VNC/reset changes
 sudo /System/Library/CoreServices/RemoteManagement/ARDAgent.app/Contents/Resources/kickstart -restart -agent -console
 sudo /System/Library/CoreServices/RemoteManagement/ARDAgent.app/Contents/Resources/kickstart -activate
+
 # --- CLOUDFLARE TUNNEL SETUP ---
+# Install cloudflared
 brew install cloudflared
-# Chạy tunnel với token cứng
-cloudflared tunnel run --token rqyT-nLv0w0Natm1mm2ufnFQHgZQI6E5IKSSWcRU &
+
+# Configure and start cloudflared using the token passed as argument $1
+# Dấu & ở cuối để chạy ẩn, giúp GitHub Action không bị treo ở bước này
+cloudflared tunnel run --token $1 &
